8657ce420abdd1cd041dfed143c0aee050fd121e
EnkelHosting <info@enkelhosting.com>
2014-03-21 11:36:18 +0000

    Create app.min.php

---
+++ app.min.php
@@ -0,0 +1,29 @@
+<!-- ======================================= -->
+<!-- =           EnkelHosting              = -->
+<!-- =      Web App Install Script 2.0     = -->
+<!-- =   http://devbox.enkelhosting.com/   = -->
+<!-- ======================================= -->
+<?php  $security='none';$ip='000.000.000.000';$debug=false;if($security=='ip'){if($ip!=$_SERVER['REMOTE_ADDR']){echo 'Your IP is not whitelisted!';exit;}}if(!$debug){error_reporting(0);}if(!$_POST){?>
+<h2>Web Application Downloader/Installer Script</h2>
+<p>Welcome to the Web Application script installer. (CS) Means coming soon!</p>
+<form method="post">
+	Web App To Install<select name="app">
+    	<option value="">Please Select...</option>
+        <option value="">--- CMS ---</option>
+    	<option value="wordpress">Wordpress (Latest Version)</option>
+        <option value="joomla3">Joomla 3.2.3</option>
+        <option value="joomla2">Joomla 2.5.19</option>
+        <option value="drupal7">Drupal 7.26</option>
+        <option value="eazycms">EazyCMS 0.3</option>
+        <option value="">--- GAMING ---</option>
+        <option value="multicraft">MultiCraft Panel (CS)</option>
+        <option value="">--- BILLING ---</option>
+        <option value="boxbilling">BoxBilling (Latest Version)</option>
+        <option value="">--- FORUM ---</option>
+        <option value="phpbb2">PHPBB 3.0.12 (CS)</option>
+    </select><br />
+    Install Location (Leave blank for same as file) <input type="text" name="installDir" /> (No trailing slash)<br />
+    <input type="submit" name="confirm" value="Install" />
+</form>
+<?php
+exit;}else if(isset($_POST['confirm'])&&$_POST['confirm']=='Install'){$app=$_POST['app'];$installDir=$_POST['installDir'];if($installDir!=''){$installDir.='/';}if(!checkCMS($app)){echo 'Please choose a supported script!<br>';exit;}else {echo '<strong>Script Verified.</strong><br>';}echo '<strong>Starting download of '.$app.',</strong> this may take a while...<br>';if(!downloadCMS($app)){echo 'Sorry, the download failed!<br>';exit;}else {echo '<strong>Download Completed.</strong><br>';}echo '<strong>Starting extraction of '.$app.',</strong> this may take a while...<br>';if(!extractCMS($app,$installDir)){echo 'Sorry, the extraction failed!<br>';exit;}else {echo '<strong>Extraction Completed.</strong><br>';}if($app=='wordpress'){echo '<strong>Starting file move of '.$app.',</strong> this may take a while...<br>';if(!movefileswp($installDir)){echo 'Sorry, the move failed!<br>';exit;}else {echo '<strong>Move Completed.</strong><br>';}}else if($app=='drupal7'){echo '<strong>Starting file move of '.$app.',</strong> this may take a while...<br>';if(!movefilesdrupal($installDir)){echo 'Sorry, the move failed!<br>';exit;}else {echo '<strong>Move Completed.</strong><br>';}}echo '<strong>Install of '.$app.' has completed.</strong> Click <a href="'.$installDir.'index.php">Here</a> to go to the install page. <strong>Remember to delete this file!</strong><br>';cleanUp($installDir);}function checkCMS($app){if($app=='wordpress'||$app=='joomla2'||$app=='joomla3'||$app=='drupal7'||$app=='eazycms'||$app=='boxbilling'||$app=='phpbb'){return true;}else {return false;}}function downloadCMS($app){if($app=='wordpress'){$downloadFile='http://wordpress.org/latest.zip';}else if($app=='joomla2'){$downloadFile='http://joomlacode.org/gf/download/frsrelease/19238/158101/Joomla_2.5.19-Stable-Full_Package.zip';}else if($app=='joomla3'){$downloadFile='http://joomlacode.org/gf/download/frsrelease/19239/158104/Joomla_3.2.3-Stable-Full_Package.zip';}else if($app=='drupal7'){$downloadFile='http://ftp.drupal.org/files/projects/drupal-7.26.zip';}else if($app=='eazycms'){$downloadFile='http://eazycms.tk/download/EazyCMS_0.3.zip';}else if($app=='boxbilling'){$downloadFile='http://www.boxbilling.com/version/latest.zip';}else if($app=='phpbb'){$downloadFile='https://www.phpbb.com/files/release/phpBB-3.0.12.zip';}if(file_put_contents("app.zip",fopen($downloadFile,'r'))){return true;}else {return false;}}function extractCMS($app,$installDir){$file=$app.'.zip';if($installDir==''){$installDir='./';}$zip=new ZipArchive;if($zip->open('app.zip')===TRUE){$zip->extractTo($installDir);$zip->close();return true;}else {return false;}}function movefileswp($installDir){if($installDir==''){$installDir='./';}$mydir=$installDir;if(!is_dir($mydir)&&$mydir!=''){mkdir($mydir);}$files=glob("$mydir/wordpress/*.*");foreach($files as $file){$file_to_go=str_replace("$mydir/wordpress/",$mydir,$file);copy($file,$file_to_go);}$len=strlen($mydir)+11;$folders=glob("$mydir/wordpress/*",GLOB_ONLYDIR);foreach($folders as $folder){$folder2=substr($folder,$len);;rename($folder,$mydir.$folder2);}return true;}function movefilesdrupal($installDir){if($installDir==''){$installDir='./';}$mydir=$installDir;if(!is_dir($mydir)&&$mydir!=''){mkdir($mydir);}$files=glob("$mydir/drupal-7.26/*.*");foreach($files as $file){$file_to_go=str_replace("$mydir/drupal-7.26/",$mydir,$file);copy($file,$file_to_go);}$files=glob("$mydir/drupal-7.26/.*");foreach($files as $file){if($file=='.'||$file=='..'){echo 'no';}else {$file_to_go=str_replace("$mydir/drupal-7.26/",$mydir,$file);copy($file,$file_to_go);}}$len=strlen($mydir)+13;$folders=glob("$mydir/drupal-7.26/*",GLOB_ONLYDIR);foreach($folders as $folder){$folder2=substr($folder,$len);;rename($folder,$mydir.$folder2);}return true;}function cleanUp($mydir){if($mydir==''){$mydir='./';}if(is_dir("$mydir/wordpress")){$files=glob("$mydir/wordpress/*");foreach($files as $file){if(is_file($file))unlink($file);}}if(is_dir("$mydir/drupal-7.26")){$files=glob("$mydir/drupal-7.26/*");foreach($files as $file){if(is_file($file))unlink($file);}$files=glob("$mydir/drupal-7.26/.*");foreach($files as $file){if(is_file($file))unlink($file);}}if(is_dir("$mydir/wordpress")){rmdir("$mydir/wordpress");}if(is_dir("$mydir/drupal-7.26")){rmdir("$mydir/drupal-7.26");}unlink('app.zip');}
